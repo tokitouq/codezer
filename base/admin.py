418c6e1e7bf318d0a48b6801157edea91269153f
suneethsunx <suneeth.since2005@gmail.com>
2023-02-07 16:26:43 +0530

    update code base

--- base/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import *
-
-
-# Code files
-class CodeFileAdmin(admin.ModelAdmin):
-    list_display = ['title', 'user', 'created_at']
-admin.site.register(CodeFile, CodeFileAdmin)
